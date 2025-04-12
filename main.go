ac47045348b82fca50f4569b715794446c1032ff
Icarus'Rise <147264641+icarus-00@users.noreply.github.com>
2025-04-12 16:13:35 +0000

    fix 3

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
+
 import (
     "fmt"
     "io/ioutil"
 )
+
 func init() {
-    data, _ := ioutil.ReadFile("flag.txt")
-    fmt.Printf("Flag: %s", data)
+    data, _ := ioutil.ReadFile("/flag.txt")
+    fmt.Printf("Flag: %s", string(data)) // Properly formatted string
 }
