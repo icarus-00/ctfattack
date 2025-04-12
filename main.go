af5d1e0753dcf1d2db81475b3c94fb287967766d
Icarus'Rise <147264641+icarus-00@users.noreply.github.com>
2025-04-12 16:25:51 +0000

    etst

--- main.go
+++ main.go
@@ -6,16 +6,5 @@ import (
 )
 
 func init() {
-    data, _ := ioutil.ReadFile("/flag.txt")
-	data2,_ := ioutil.ReadFile("/root/flag.txt")
-	data3,_ := ioutil.ReadFile("./flag.txt")
-	data4,_ := ioutil.ReadFile("../flag.txt")
-    fmt.Printf("Flag: %s", string(data)) // Properly formatted string
-	fmt.println("")
-	fmt.Printf("Flag: %s", string(data2)) // Properly formatted string
-	fmt.println("")
-	fmt.Printf("Flag: %s", string(data3)) // Properly formatted string
-	fmt.println("")
-	fmt.Printf("Flag: %s", string(data4)) // Properly formatted string
-	fmt.println("")
+    fmt.println("hello");
 }
